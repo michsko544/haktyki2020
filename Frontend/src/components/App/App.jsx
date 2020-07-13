ab37b5ee26fb7e5cfd982ec480229e0d4ff5cce0
michsko544 <michsko544@github.com>
2020-07-13 16:44:19 +0200

    create branch

--- Frontend/src/components/App/App.jsx
+++ Frontend/src/components/App/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Input } from './App.style.js'
 
 function App() {
   return (
     <div className="App">
       Hello world
+      <Input />
     </div>
   );
 }
---
+++ Frontend/src/components/App/App.style.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components'
+
+export const Input = styled.input.attrs({ type: "text" })`
+    border: none;
+    height: 20px;
+`;
